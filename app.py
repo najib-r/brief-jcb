49647a576074ede2251ef311a8b419aab3c4e153
ysosrss <najib72645@gmail.com>
2022-11-27 17:24:07 +0800

    Remove number search

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_session import Session
-from scrape import pyJobnumberSearch, pyJobdetailsFetch, pyJoblinksfetch, pyJobcompanyNames
+from flask import Flask, render_template
+from scrape import pyJobdetailsFetch, pyJoblinksfetch, pyJobcompanyNames
 
 
 app = Flask(__name__)
@@ -8,11 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     default = 'sort=modified-&q=&delta=75'
-    number = pyJobnumberSearch(default)
     jobs = pyJobdetailsFetch(default)
     links = pyJoblinksfetch(default)
     names = pyJobcompanyNames(default)
-    return render_template("index.html", number=number, jobs=jobs, links=links, names=names)
+    return render_template("index.html", jobs=jobs, links=links, names=names)
 
 if __name__ == '__main__':
     app.run()
