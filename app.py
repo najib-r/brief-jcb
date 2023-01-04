743c1820f0229fd8bb18b539602c71643aa47eb8
ysosrss <najib72645@gmail.com>
2023-01-04 20:21:34 +0800

    Add if else to handle error

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ def index():
     jobs = fetch_details(default)
     if jobs == "error":
         return render_template("error.html")
-    links = fetch_links(default)
-    names = fetch_companynames(default)
-    return render_template("index.html", jobs=jobs, links=links, names=names)
+    else:
+        links = fetch_links(default)
+        names = fetch_companynames(default)
+        return render_template("index.html", jobs=jobs, links=links, names=names)
 
 if __name__ == '__main__':
     app.run() 
