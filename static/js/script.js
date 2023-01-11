f471f68c8fee2c0e8681182a71cef9402dfde957
ysosrss <najib72645@gmail.com>
2023-01-11 18:01:55 +0800

    Add offline page cache

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,8 @@
-if('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/static/sw.js')
-             .then(function(registration) {
-             console.log('Service Worker Registered');
-             return registration;
-    })
-    .catch(function(err) {
-      console.error('Unable to register service worker.', err);
-    });
-    navigator.serviceWorker.ready.then(function(registration) {
-      console.log('Service Worker Ready');
-    });
-  });
-}
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/static/sw.js");
+  }
+});
 
 // Check to see if Media-Queries are supported
 if (window.matchMedia) {
