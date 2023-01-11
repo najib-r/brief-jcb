71a77ff8810007b451171e1856bc5afdb78068dc
ysosrss <najib72645@gmail.com>
2023-01-11 17:45:09 +0800

    Add service worker

---
+++ static/js/script.js
@@ -0,0 +1,24 @@
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/static/sw.js')
+             .then(function(registration) {
+             console.log('Service Worker Registered');
+             return registration;
+    })
+    .catch(function(err) {
+      console.error('Unable to register service worker.', err);
+    });
+    navigator.serviceWorker.ready.then(function(registration) {
+      console.log('Service Worker Ready');
+    });
+  });
+}
+
+// Check to see if Media-Queries are supported
+if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      // Dark
+      document.getElementById('themeColor').content = "#121212";
+    }
+}
