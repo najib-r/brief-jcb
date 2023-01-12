30f24d2eb4a97a31fa0db5fd24f7f6bcfb892f24
ysosrss <najib72645@gmail.com>
2023-01-12 10:36:47 +0800

    Add delay between requests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from scrape import fetch_details, fetch_links, fetch_companynames
+import time
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ def index():
         return render_template("error.html")
     else:
         links = fetch_links(default)
+        time.sleep(1)
         names = fetch_companynames(default)
         return render_template("index.html", jobs=jobs, links=links, names=names)
         
