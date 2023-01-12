e1c929daaabecdd5570b9ce90748976d101989ae
ysosrss <najib72645@gmail.com>
2023-01-12 11:03:32 +0800

    remove service worker registration

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,3 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//       navigator.serviceWorker.register("../sw.js").then(function(registration) {
-//           // Registration was successful
-//           console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//           // registration failed :(
-//           console.log('ServiceWorker registration failed: ', err);
-//       });
-//   });
-// }
-
 // Check to see if Media-Queries are supported
 if (window.matchMedia) {
     // Check if the dark-mode Media-Query matches
