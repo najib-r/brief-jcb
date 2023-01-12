6d9f54b860b96963409d7caae990d2f200ebda19
ysosrss <najib72645@gmail.com>
2023-01-12 10:46:03 +0800

    Remove sw.js

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from scrape import fetch_details, fetch_links, fetch_companynames
-import time
 
 
 app = Flask(__name__)
@@ -13,13 +12,8 @@ def index():
         return render_template("error.html")
     else:
         links = fetch_links(default)
-        time.sleep(1)
         names = fetch_companynames(default)
         return render_template("index.html", jobs=jobs, links=links, names=names)
-        
-# @app.route('/sw.js', methods=['GET'])
-# def sw():
-#     return app.send_static_file('sw.js')
 
 if __name__ == '__main__':
     app.run() 
