95a09f6b744b74f2ee4ff0eb2fffacc976f77f87
ysosrss <najib72645@gmail.com>
2023-01-12 10:08:23 +0800

    Add service worker

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,14 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//       navigator.serviceWorker.register("../sw.js").then(function(registration) {
-//           // Registration was successful
-//           console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//           // registration failed :(
-//           console.log('ServiceWorker registration failed: ', err);
-//       });
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+      navigator.serviceWorker.register("../sw.js").then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
 
 // Check to see if Media-Queries are supported
 if (window.matchMedia) {
