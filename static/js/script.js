02caebbb35d06268071110b3074deed0ff4f8e94
najib-r <najib72645@gmail.com>
2023-05-07 11:56:26 +0800

    add search functionality

--- static/js/script.js
+++ static/js/script.js
@@ -24,3 +24,58 @@ if (window.matchMedia) {
     });
   }
 })();
+
+function myFunction() {
+  let input, filter, found, table, tr, td, i, j;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+          if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+              found = true;
+          }
+      }
+      if (found) {
+          tr[i].style.display = "";
+          found = false;
+      } else {
+          tr[i].style.display = "none";
+      }
+  }
+}
+
+let input = document.getElementById("myInput");
+input.addEventListener('search', myFunction)
+input.addEventListener('keyup', function(e) {
+  let input, filter, found, table, tr, td, i, j;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+          if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+              found = true;
+          }
+      }
+      if (found) {
+          tr[i].style.display = "";
+          found = false;
+      } else {
+          tr[i].style.display = "none";
+      }
+  }
+  if (e.which === 13) {
+    console.log('Enter key pressed.')
+    input.blur()
+  }
+})
+
+let search = document.getElementById("searchBtn");
+search.addEventListener('click', () => {
+  input.blur();
+})
